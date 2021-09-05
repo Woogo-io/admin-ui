9be5a443495652e5182e55241022d594fac776ab
GautheyValentin <18257114+GautheyValentin@users.noreply.github.com>
2021-09-05 13:35:55 +0200

    :art: Update modal style

--- src/theme/default.ts
+++ src/theme/default.ts
@@ -96,8 +96,8 @@ export default {
   modal: {
     background: 'opacity-25 fixed inset-0 z-40 bg-black',
     base:
-      'justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none',
-    sizer: 'relative w-full my-6 mx-auto',
+      'overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none',
+    sizer: 'relative w-full mt-20 mx-auto',
     content: 'border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-900 outline-none focus:outline-none',
   },
   // ModalHeader
