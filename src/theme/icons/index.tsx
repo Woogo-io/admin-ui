5cebe277b7d9fa5e1d41df85501aa34eb425b223
GautheyValentin <18257114+GautheyValentin@users.noreply.github.com>
2021-09-05 18:47:23 +0200

    :sparkles: New Icon

--- src/theme/icons/index.tsx
+++ src/theme/icons/index.tsx
@@ -176,6 +176,12 @@ export const CloseIcon: FC<SvgProps> = ({ className }: SvgProps) => (
   </svg>
 );
 
+export const ValidIcon: FC<SvgProps> = ({ className }: SvgProps) => (
+  <svg viewBox="0 0 24 24" className={className} fill="currentColor">
+    <path d="m9.707 19.121c-.187.188-.442.293-.707.293s-.52-.105-.707-.293l-5.646-5.647c-.586-.586-.586-1.536 0-2.121l.707-.707c.586-.586 1.535-.586 2.121 0l3.525 3.525 9.525-9.525c.586-.586 1.536-.586 2.121 0l.707.707c.586.586.586 1.536 0 2.121z" />
+  </svg>
+);
+
 const Icons = {
   BellIcon,
   MenuIcon,
@@ -191,6 +197,7 @@ const Icons = {
   ArrowLeft,
   ArrowRight,
   CloseIcon,
+  ValidIcon,
 };
 
 export type IconType = 'BellIcon'
@@ -206,7 +213,8 @@ export type IconType = 'BellIcon'
 | 'DeleteIcon'
 | 'ArrowLeft'
 | 'ArrowRight'
-| 'CloseIcon';
+| 'CloseIcon'
+| 'ValidIcon';
 
 type IconProps = {
   icon: IconType;
