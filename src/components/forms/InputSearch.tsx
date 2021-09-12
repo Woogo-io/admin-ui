64c9f0fd2eb51531885a493c8e48a5b54424a94d
GautheyValentin <18257114+GautheyValentin@users.noreply.github.com>
2021-09-12 22:32:49 +0200

    :bug: Fix className on input search

--- src/components/forms/InputSearch.tsx
+++ src/components/forms/InputSearch.tsx
@@ -6,12 +6,12 @@ import { SearchIcon } from '../../theme/icons';
 export type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
 const InputSearch = ({ className, ...props }: InputProps) => (
-  <div className={theme.header.search.wrapper}>
+  <div className={clsx(theme.header.search.wrapper, className)}>
     <div className={theme.header.search.iconContainer}>
       <SearchIcon className={theme.header.search.icon} aria-hidden="true" />
     </div>
     <input
-      className={clsx(theme.input.search, className)}
+      className={clsx(theme.input.search)}
       {...props}
     />
   </div>
