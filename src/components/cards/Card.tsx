78e121299e8f3c2148e38ceb6ab2cab691fd6413
GautheyValentin <18257114+GautheyValentin@users.noreply.github.com>
2021-09-12 20:39:49 +0200

    :sparkles: Add missing classname on card

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -5,16 +5,19 @@ import theme from '../../theme/default';
 
 export type CardProps = {
   colored?: boolean;
+  className?: string
 };
 
 const Card: FC<PropsWithChildren<CardProps>> = ({
   colored,
   children,
+  className,
 }: PropsWithChildren<CardProps>) => (
   <div
     className={clsx(
       theme.card.base,
       colored ? theme.card.colored : theme.card.default,
+      className,
     )}
   >
     {children}
