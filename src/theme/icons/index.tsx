500ce19a38ba12cdd8c4e79fa40274b0e6655cc0
GautheyValentin <18257114+GautheyValentin@users.noreply.github.com>
2021-11-19 17:07:21 +0100

    :sparkles: Add icon

--- src/theme/icons/index.tsx
+++ src/theme/icons/index.tsx
@@ -304,6 +304,18 @@ export const QuoteRightIcon: FC<SvgProps> = ({ className }: SvgProps) => (
   </svg>
 );
 
+export const ShoppingCartIcon: FC<SvgProps> = ({ className }: SvgProps) => (
+  <svg className={className} fill="currentColor" viewBox="0 0 576 512">
+    <path fill="currentColor" d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z" />
+  </svg>
+);
+
+export const VestIcon: FC<SvgProps> = ({ className }: SvgProps) => (
+  <svg className={className} fill="currentColor" viewBox="0 0 448 512">
+    <path fill="currentColor" d="M437.252,239.877,384,160V32A32,32,0,0,0,352,0H320a23.982,23.982,0,0,0-13.312,4.031l-25,16.672a103.794,103.794,0,0,1-115.376,0l-25-16.672A23.982,23.982,0,0,0,128,0H96A32,32,0,0,0,64,32V160L10.748,239.877A64,64,0,0,0,0,275.377V480a32,32,0,0,0,32,32H192V288a31.987,31.987,0,0,1,1.643-10.119L207.135,237.4,150.188,66.561A151.579,151.579,0,0,0,224,86.234a151.565,151.565,0,0,0,73.811-19.668L224,288V512H416a32,32,0,0,0,32-32V275.377A64,64,0,0,0,437.252,239.877ZM63.5,272.484a12.01,12.01,0,0,1,17-16.968l15.5,15.5,15.5-15.5a12.01,12.01,0,0,1,17,16.968L112.984,288,128.5,303.516a12.01,12.01,0,0,1-17,16.968L96,304.984l-15.5,15.5a12.01,12.01,0,0,1-17-16.968L79.016,288ZM96,456a40,40,0,1,1,40-40A40,40,0,0,1,96,456ZM359.227,335.785,310.7,336a6.671,6.671,0,0,1-6.7-6.7l.215-48.574A24.987,24.987,0,0,1,331.43,256.1c12.789,1.162,22.129,12.619,22.056,25.419l-.037,5.057,5.051-.037c12.826-.035,24.236,9.275,25.4,22.076A24.948,24.948,0,0,1,359.227,335.785Z" />
+  </svg>
+);
+
 const Icons = {
   BellIcon,
   MenuIcon,
@@ -339,42 +351,11 @@ const Icons = {
   StopIcon,
   CodeBlockIcon,
   QuoteRightIcon,
+  ShoppingCartIcon,
+  VestIcon,
 };
 
-export type IconType = 'BellIcon'
-| 'MenuIcon'
-| 'MoonIcon'
-| 'SearchIcon'
-| 'SunIcon'
-| 'ForbiddenIcon'
-| 'HomeIcon'
-| 'PageIcon'
-| 'LanguageIcon'
-| 'EditIcon'
-| 'DeleteIcon'
-| 'ArrowLeft'
-| 'ArrowRight'
-| 'CloseIcon'
-| 'ValidIcon'
-| 'ComponentIcon'
-| 'MetaIcon'
-| 'MediaIcon'
-| 'AddIcon'
-| 'AngleDownIcon'
-| 'TableListIcon'
-| 'BoxIcon'
-| 'BoldIcon'
-| 'CodeIcon'
-| 'OrderIcon'
-| 'ItalicIcon'
-| 'ListOlIcon'
-| 'ListUlIcon'
-| 'TerminalIcon'
-| 'UnderlineIcon'
-| 'UsersIcon'
-| 'StopIcon'
-| 'CodeBlockIcon'
-| 'QuoteRightIcon';
+export type IconType = keyof typeof Icons;
 
 type IconProps = {
   icon: IconType;
